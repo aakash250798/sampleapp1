5a4aa2b3ece85ddb1b5fe9be37446f4aec44c792
aakash250798 <aakash250798@gmail.com>
2021-09-20 21:36:37 +0530

    first commit

---
+++ sampleapp/src/main/java/org/capg/demo/BootClass.java
@@ -0,0 +1,11 @@
+package org.capg.demo;
+
+public class BootClass {
+
+	public static void main(String[] args) {
+		System.out.print("Hello! from sts..!!!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
